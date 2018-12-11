db68cf70228af5963cc1c389751b053b3d976a0f
scottbeebiwan <scottbeebiwan@yahoo.com>
2018-12-10 19:44:35 -0800

    nothing actually useful

---
+++ RAcc_Server/RAcc_Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAcc_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("== RAccThing Server == Written by ScottBeebiWan ==");
+            Console.WriteLine("Opening socket...");
+            
+        }
+    }
+}
